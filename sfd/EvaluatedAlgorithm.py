44745c8b3da6fc4eeecf5ea7c5571c89583cb8c5
Santosh Kumar <Singh.santosh2702@gmail.com>
2019-01-13 12:19:17 +0530

    algo

--- sfd/EvaluatedAlgorithm.py
+++ sfd/EvaluatedAlgorithm.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu May  3 10:45:33 2018
 
-@author: Frank
-"""
 from RecommenderMetrics import RecommenderMetrics
 from EvaluationData import EvaluationData
 
